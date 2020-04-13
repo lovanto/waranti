0f8d5cc0f907a7de07363c7fd3432a10fb58b17c
lovanto <rifkylovanto@gmail.com>
2020-04-13 18:24:27 +0700

    routes updated

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,6 +50,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'Auth';
+$route['auth/login'] = 'Auth/login';
+$route['page/welcome'] = 'page/welcome';
 $route['Secret'] = 'main';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
