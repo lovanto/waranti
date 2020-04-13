a809f8322828420c34533712951863b2f65fcf93
lovanto <rifkylovanto@gmail.com>
2020-04-13 18:19:48 +0700

    adaw

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('form','url', 'date', 'number', 'security');
+$autoload['helper'] = array('url');
 
 /*
 | -------------------------------------------------------------------
